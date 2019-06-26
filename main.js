5dc6cf34f9b98d403092259e58a1c2e9bce9c2d1
Vyacheslav <vyacheslav.code@gmail.com>
2019-06-26 18:15:42 +0200

    changed logic based on svg elements

--- main.js
+++ main.js
@@ -1,24 +1,42 @@
 const slider = document.getElementById('slider');
-const stripe = document.getElementById('stripe-image');
-const separator = document.getElementById('separator');
+const blueStripe = document.getElementById('blue-stripe');
+const blueArrow = document.getElementById('blue-arrow');
+const greenStripe = document.getElementById('green-stripe');
+const greenArrow = document.getElementById('green-arrow');
 const text1 = document.getElementById('text-1');
 const text2 = document.getElementById('text-2');
 const text3 = document.getElementById('text-3');
 
-const stripeSpeed = 1.35;
-const text1OpacitySpeed = 0.05;
-const text2OpacitySpeed = 0.025;
-const text3OpacitySpeed = 0.1;
+const speed = 0.53;
 
 slider.addEventListener('mousemove', function () {
     const value = slider.value;
-    stripe.style.marginRight = `-${value*stripeSpeed}%`;
-    if (value < 20) {
-        text1.style.opacity = `${1 - text1OpacitySpeed*value}`;
-    } else if (value >= 20 && value < 100) {
-        text3.style.opacity = `${1 - text2OpacitySpeed*(value - 20)}`;
-    }
-    if ( value >= 70) {
-        text2.style.opacity = `${text3OpacitySpeed*(value - 70)}`
+    blueStripe.style.left = `${speed*slider.value - 54.6}%`;
+    blueArrow.style.left = `${speed*slider.value - 2.6}%`;
+    greenStripe.style.left = `${speed*slider.value}%`;
+    greenArrow.style.left = `${speed*slider.value - 2.6}%`;
+    if(value > 31 && value < 60) {
+        text1.style.zIndex = '0';
+        text2.style.zIndex = '996';
+        text3.style.zIndex = '999';
+        greenArrow.style.zIndex = '998';
+        greenStripe.style.zIndex = '998';
+        blueArrow.style.zIndex = '995';
+        blueStripe.style.zIndex = '995';
+    } else if(value >= 60) {
+        text2.style.zIndex = '999';
+        text3.style.zIndex = '996';
+        greenArrow.style.zIndex = '995';
+        greenStripe.style.zIndex = '995';
+        blueArrow.style.zIndex = '998';
+        blueStripe.style.zIndex = '998';
+    } else {
+        text1.style.zIndex = '';
+        text2.style.zIndex = '';
+        text3.style.zIndex = '';
+        greenArrow.style.zIndex = '';
+        greenStripe.style.zIndex = '';
+        blueArrow.style.zIndex = '';
+        blueStripe.style.zIndex = '';
     }
 })
